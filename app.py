7b7a47117b2b64f2855790c49ea4dd4abbba9672
DunkanAydaho <35607124+DunkanAydaho@users.noreply.github.com>
2018-01-20 18:54:40 +0600

    new_comments

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 #PY Script
 book = int(input("Enter number of books:"))
-
+#Check and reply
 if book >= 5:
   print("Good choise")
 elif book < 5:
   print("Take more!")
 else:
   print("What da fuk a u doin brah?")
+#Need new function here
