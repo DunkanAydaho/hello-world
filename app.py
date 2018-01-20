e8834e48024c7778d75facfa5b3c514c7f20e23d
DunkanAydaho <35607124+DunkanAydaho@users.noreply.github.com>
2018-01-20 19:15:46 +0600

    price and calc added

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 #PY Script
 book = int(input("Enter number of books:"))
+price = 50
+total = book * price
 #Check and reply
 if book >= 5:
-  print("Good choise")
+  print("Good choise, your total sum: ${}".format(total))
 elif book < 5:
   print("Take more!")
 else:
